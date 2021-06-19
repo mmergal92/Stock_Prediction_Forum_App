f303fbc9f3eb2e57fd8f2d0ea628bc8cb0d19715
mmergal92 <79428668+mmergal92@users.noreply.github.com>
2021-06-18 20:07:24 -0400

    adding formsbut not done yet

---
+++ src/models/comments.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+const url = "http://stock-prediction-forum-app.herokuapp.com/user/comments"
+
+class UserComments {
+    static all (){
+        return axios.get(url);
+    }
+    static categories(){
+        return axios.get(`http://stock-prediction-forum-app.herokuapp.com/user/comments`)
+    }
+    static create(formData){
+        return axios.post(`http://stock-prediction-forum-app.herokuapp.com/user/comments`, formData)
+    }
+    // static questionsByCategory(category){
+    //     return axios.get(`https://pure-ocean-22147.herokuapp.com/categories/${category}`)
+    // }
+    // static byId(id){
+    //     return axios.get(`https://pure-ocean-22147.herokuapp.com/questions/${id}`)
+    // }
+    // static addQuestion(obj){
+    //     return axios.post(url,obj)
+    // }
+    // static update(id,obj){
+    //     return axios.put(`https://pure-ocean-22147.herokuapp.com/questions/${id}`,obj)
+    // }
+    // static delete(id){
+    //     return axios.delete(`https://pure-ocean-22147.herokuapp.com/questions/${id}`)
+    // }
+    // static findByUserId(id){
+    //     const userId = {userId: id}
+    //     return axios.post(`https://pure-ocean-22147.herokuapp.com/questions/findById`,userId)
+    // }
+}
+
+export default UserComments;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,42 @@
 import React from 'react'
-
+import Form from "@rjsf/core"
 
 
 // Components
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import UserComments from '../models/comments';
 
 function Home() {
-    let x;
-    console.log(x)
+    const schema = {
+        title: "Comments",
+        type: "object",
+        required: ["comment"],
+        properties: {
+          symbol: {type: "string", title: "Company Symbol"},
+          date: {type: "string", title: "Date", default: Date.now},
+          comment: {type: "string", title: "Comment", default: "Write your new comment here."},
+          username: {type: "string", title: "Username",}
+        }
+      };
+      const log = (type) => console.log.bind(console, type);
+    
+      const onSubmitTest = async({formData}) => {
+          console.log(formData)
+          await UserComments.create(formData)
+          window.location.reload()
+      }
+
+    // let x;
+    // console.log(x)
+
     return (
         <div>
             <Header/>
-            Home Page
+            <Form schema={schema}
+        onChange={log("changed")}
+        onSubmit={onSubmitTest}
+        onError={log("errors")} />
             <Footer/>
         </div>
     )
