681674051e033e69ef8f4a13c9d5c972d5d3caca
dev <welcome>
2021-06-25 11:53:01 -0700

    created simple algorithm for stock prediction

--- src/components/Dashboard/StockTweets.js
+++ src/components/Dashboard/StockTweets.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
 
 function StockTweets() {
+    var posSym  = (window.location.href).split("/stocks/").pop().replace('?', '');
+// console.log(posSym)
+let sym = posSym
+
+
+
     return (
-        <div>
-            
+        <div className = "stocktweets">
+            <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="MarketWatch"
+                options={{ height: 300 }}
+            />
         </div>
+        
     )
 }
 
 export default StockTweets
+
+
