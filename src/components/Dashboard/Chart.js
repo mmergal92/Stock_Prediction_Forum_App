2ab084a34cb690fb572bce658addc69cbf5708a1
mmergal92 <79428668+mmergal92@users.noreply.github.com>
2021-06-26 01:50:56 -0400

    added heroku links

--- src/components/Dashboard/Chart.js
+++ src/components/Dashboard/Chart.js
@@ -1,67 +1,29 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Line } from 'react-chartjs-2';
-
-const ChartGraph = () => {
-// var posSym  = (window.location.href).split("/stocks/").pop().replace('?', '');
-// // console.log(posSym)
-// let sym = posSym
-// console.log(sym)
-
-// const url = "https://stock-prediction-forum-backend.herokuapp.com/" + "api/stock/price/" + sym;
-// console.log(url)
-
-// const [chartData, setChartData] = useState('null');
-
-// const getChartData = async() =>{
-//   const response = await fetch(url);
-//   const chartdataset = await response.json();
-//   console.log(chartdataset)
-//   setChartData(chartdataset)
-// }
-// React.useEffect(() =>{
-//   getChartData();
-// },[])
-
-// var excludeAfterIndex = 30;
-
-// data.reduce((mappedArray, item, index) =>{
-//   if (index > excludeAfterIndex) {
-//     mappedArray.push(<data key={index} data={item}/>)
-//   }
-//   return mappedArray;
-// },[])
-
-// const data = ()=> {
-//   for (let i=0; i<chartData.length; i++){
-//     console.log(chartData[i].close)
-//   }
-//   // labels: ['1', '2', '3', '4', '5', '6'],
-//   // datasets: [
-//   //   {
-//   //     label: '# of Votes',
-//   //     data: [chartData],
-//   //     fill: false,
-//   //     backgroundColor: 'rgb(255, 99, 132)',
-//   //     borderColor: 'rgba(255, 99, 132, 0.2)',
-//   //   },
-//   // ],
-// };
-// data()
-
-// const options = {
-//   scales: {
-//     yAxes: [
-//       {
-//         ticks: {
-//           beginAtZero: true,
-//         },
-//       },
-//     ],
-//   },
-// };
-
-// const Chart = () => (
-return(
+const data = {
+  labels: ['1', '2', '3', '4', '5', '6'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      fill: false,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+    },
+  ],
+};
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+const Chart = () => (
   <>
     <div className='header'>
       <h1 className='title'>Line Chart</h1>
@@ -75,15 +37,8 @@ return(
       </div>
     </div>
     <div className="chartdiv">
-    {/* <Line data={data} /> */}
+    <Line data={data} />
     </div>
-    
   </>
 );
-
-// const loading =()=>{
-//   return <h1>Loading ...</h1>
-// }
-// return chartData ? Chart(): loading();
-}
-export default ChartGraph;
+export default Chart;
